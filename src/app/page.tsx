82bed9a16f80ea7546ce57d47d000708815dfa85
Malik Teague <malikisiah214@gmail.com>
2023-10-10 19:02:46 -0700

    adding some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import Hero from "@/components/ui/hero";
 
 export default function Page() {
-  return (
-    <>
-      <h1 className="text-center pt-20 font-bold ">Hello World</h1>
-      <div className="pt-32 flex items-center justify-center align-middle gap-6">
-        <Button onClick={() => console.log("I've been clicked!")}>
-          Click Me
-        </Button>
-        <Button variant={"secondary"}> No, Click Me</Button>
-      </div>
-    </>
-  );
+  return <Hero />;
 }
